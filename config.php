e06bfb8ecef376c04db5e810c2b616b0a8045b53
JRejowski <jan.rejowski@student.pk.edu.pl>
2023-01-20 19:29:42 +0100

    PDO, Database connection but something is broken, will fix later

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+const  USERNAME = 'postgres';
+const PASSWORD = '123';
+const HOST = 'localhost';
+const DATABASE = 'fitfreak';
--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,15 +2,26 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 class SecurityController extends AppController
 {
     public function login()
     {
-        $user = new \models\User('major@bombas.pl','haslo','Wojciech','Suchodolski');
+        $userRepository = new UserRepository();
 
+        if(!$this->isPost()){
+            return $this->render('login');
+        }
 
         $email = $_POST["email"];
         $password = $_POST["password"];
+
+        $user = $userRepository->getUser($email);
+
+        if(!user){
+            return $this ->render('login', ['messages' => ['User does not exist!']]);
+        }
+
 
         if($user->getEmail() !== $email)
         {
